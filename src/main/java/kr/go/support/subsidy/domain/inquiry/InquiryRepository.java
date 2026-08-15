627e2f508cb2b81b5ce5f675c90f88513250cd38
Soil0220 <kkkyj0220@naver.com>
2026-08-15 21:14:07 +0900

    Inquiry 전체조회 기능 추가

--- src/main/java/kr/go/support/subsidy/domain/inquiry/InquiryRepository.java
+++ src/main/java/kr/go/support/subsidy/domain/inquiry/InquiryRepository.java
@@ -1,7 +1,10 @@
 package kr.go.support.subsidy.domain.inquiry;
 
+import kr.go.support.subsidy.domain.application.Application;
 import kr.go.support.subsidy.domain.inquiry.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +12,8 @@ import java.util.Optional;
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     List<Inquiry> findByUserId(Long userId);
     Optional<Inquiry> findByIdAndUserId(Long inquiryId, Long userId);
+    @Query("select a from Inquiry a join fetch a.user")
+    List<Inquiry> findAllWithUser();
+    @Query("select a from Inquiry a join fetch a.user where a.user.id = :userId")
+    List<Inquiry> findByUserIdWithUser(@Param("userId") Long userId);
 }

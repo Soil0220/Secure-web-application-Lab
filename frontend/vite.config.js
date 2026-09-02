a9b02d930c9053be00a994d501ab44509c5ef69d
Soil0220 <kkkyj0220@naver.com>
2026-09-02 18:11:57 +0900

    취약설정(빌드시 소스맵 포함) 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //[취약한 설정] 빌드시 소스맵 포함
+  build: {sourcemap: true},
   plugins: [react()],
   server: {
     host: '127.0.0.1',
